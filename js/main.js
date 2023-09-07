e3f9f389feed16698ca733085e5ca44aab69e886
AfrinSajib <137946162+AfrinSajib@users.noreply.github.com>
2023-09-08 01:30:50 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".banner-carousel").owlCarousel({
+    items:1,
+    nav:true,
+    loop:true,
+    autoplay:true,
+  });
+});
+
+
+$(document).ready(function(){
+  $(".brand-carousel").owlCarousel({
+    items:5,
+    loop:true,
+    autoplay:true,
+  });
+});
